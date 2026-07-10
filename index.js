b726cc451d608ad3e7a05fd3117d8c2e516e3e90
B1nar <kalashnikov.j.a.y.23@gmail.com>
2026-07-10 13:47:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -149,6 +149,8 @@ function processPeriods(guilds) {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => res.send('<h1>Statbot запущен!</h1><p><a href="/dashboard">Перейти в дашборд</a></p>'));
 
 app.get('/dashboard', async (req, res) => {
